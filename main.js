2e73f0b1a69e1f630f1638ac01b1f9936caf3edb
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-26 18:49:08 +0300

    s

--- main.js
+++ main.js
@@ -18,17 +18,30 @@
 // }
 // printToLog();
 // 5
-async function doFetch (){
-    myBtr.disable =true;
-    myDiv.innerHTML='<img src = "loading_2.gif">'
+// async function doFetch (){
+//     myBtr.disable =true;
+//     myDiv.innerHTML='<img src = "loading_2.gif">'
 
+//     try{
+//         return await fetch('https://cat-fact.herokuapp.com/facts').then((item) => item.json())
+//     }catch(err){}
+//     finally{myBtr.disable =false;
+//         myDiv.innerHTML='<loading_2.gif>'}
+// }
+//  function printToLog(){
+//     doFetch ().then((result)=>console.log(result));
+//  }printToLog()
+async function doFetch(){
+    myBtr.disable =true;
+    MyDiv.innerHTML="<img src=loading_2.gif>"
     try{
-        return await fetch('https://cat-fact.herokuapp.com/facts').then((item) => item.json())
-    }catch(err){}
-    finally{myBtr.disable =false;
-        myDiv.innerHTML='<loading_2.gif>'}
+        return await fetch (' https://placekeanu.com/200/150 ').then(item =>item.json())
+    }
+    catch(err){}
+    finally{}
 }
- function printToLog(){
-    doFetch ().then((result)=>console.log(result));
- }printToLog()
+function printToScrin(){
+    doFetch().then((result) => MyDiv22.innerHTML = (result))
+
+}printToScrin()
 
