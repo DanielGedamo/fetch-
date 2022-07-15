673b0135ff8333171ba6cb1d30537b87b51957ad
DANIELGEDAMO <danielgadamo@gmail.com>
2022-07-15 19:10:20 +0300

    השלמות 2

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 1 - 2 - 3 - 4;
-// async function doFetch() {
-//   myBtr.disable = true;
-//  myDiv.innerHTML= '<img src="loading_2.gif">'
-//   try {
+async function doFetch() {
+  myBtr.disable = true;
+ myDiv.innerHTML= '<img src="loading_2.gif">'
+  try {
 
-//     return await fetch("https://moviesmern.herokuapp.com/movies/all").then(
-//       (item) => item.json()
-//     );
-//   } catch (err) {
-//   } finally {
-//     myBtr.disable = false;
-//     myDiv.innerHTML=""
-//   }
-// }
-// function printToLog() {
-//   doFetch().then((result) => console.log(result));
-// }
-// printToLog();
-// 5
-// async function doFetch() {
-//   myBtr.disable = true;
-//   myDiv.innerHTML = '<img src = "loading_2.gif">';
+    return await fetch("https://moviesmern.herokuapp.com/movies/all").then(
+      (item) => item.json()
+    );
+  } catch (err) {
+  } finally {
+    myBtr.disable = false;
+    myDiv.innerHTML=""
+  }
+}
+function printToLog() {
+  doFetch().then((result) => console.log(result));
+}
+printToLog();
+5
+async function doFetch() {
+  myBtr.disable = true;
+  myDiv.innerHTML = '<img src = "loading_2.gif">';
 
-//   try {
-//     return await fetch("https://cat-fact.herokuapp.com/facts").then((item) =>
-//       item.json()
-//     );
-//   } catch (err) 
-// {console.log(err) }
+  try {
+    return await fetch("https://cat-fact.herokuapp.com/facts").then((item) =>
+      item.json()
+    );
+  } catch (err) 
+{console.log(err) }
 
-//  finally {
-//     myBtr.disable = false;
-//     myDiv.innerHTML = "";
-//   }
-// }
-// function printToLog() {
-//   doFetch().then((result) => console.log(result));
-// }
-// printToLog();
-// async function doFetch() {
-//   myBtr.disable = true;
-//   myDiv.innerHTML = "<img src=loading_2.gif>";
-//   try {
-//     return await fetch(" https://placekeanu.com/200/150 ").then((item) =>
-//       item.json()
-//     );
-//   } catch (err) {
-//   } finally {
-//   }
-// }
-// function printToScrin() {
-//   doFetch().then((result) => (myDiv22.innerHTML = result));
-// }
-// printToScrin();
+ finally {
+    myBtr.disable = false;
+    myDiv.innerHTML = "";
+  }
+}
+function printToLog() {
+  doFetch().then((result) => console.log(result));
+}
+printToLog();
+async function doFetch() {
+  myBtr.disable = true;
+  myDiv.innerHTML = "<img src=loading_2.gif>";
+  try {
+    return await fetch(" https://placekeanu.com/200/150 ").then((item) =>
+      item.json()
+    );
+  } catch (err) {
+  } finally {
+  }
+}
+function printToScrin() {
+  doFetch().then((result) => (myDiv22.innerHTML = result));
+}
+printToScrin();
 
 
 async function getData(){
